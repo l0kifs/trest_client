c46cec0edb248b8a8cdfbdc675bd902db1c738c4
l0kifs <l0kifs91@gmail.com>
2023-01-03 15:28:19 +0300

    Add tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -2,7 +2,18 @@ from trest.rest_request import RESTRequest
 from trest.configuration.config import Config
 
 
-def test_print_one_line_request(capsys):
+def test_print_multiline_request(capsys):
+    Config.print_request_to_std_out = True
+
+    request = RESTRequest(method='GET', url='https://api.ipify.org', headers={'accept': 'text/html'})
+    response = request.send()
+
+    captured = capsys.readouterr()
+    assert len(captured.out) > 2
+    assert captured.out.count('\n') > 1
+
+
+def test_print_oneline_request(capsys):
     Config.one_line_request = True
     Config.print_request_to_std_out = True
 
@@ -14,7 +25,18 @@ def test_print_one_line_request(capsys):
     assert captured.out.count('\n') == 1
 
 
-def test_print_one_line_response(capsys):
+def test_print_multiline_response(capsys):
+    Config.print_response_to_std_out = True
+
+    request = RESTRequest(method='GET', url='https://api.ipify.org')
+    response = request.send()
+
+    captured = capsys.readouterr()
+    assert len(captured.out) > 2
+    assert captured.out.count('\n') > 1
+
+
+def test_print_oneline_response(capsys):
     Config.one_line_response = True
     Config.print_response_to_std_out = True
 
