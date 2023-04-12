9c0b5891cb084a8b6b2762cd384e7dcf6f6f02f0
l0kifs <l0kifs91@gmail.com>
2023-04-12 18:37:06 +0300

    add allure report flag

--- src/trest/rest_request.py
+++ src/trest/rest_request.py
@@ -6,6 +6,7 @@ from typing import TypeVar, Generic, Dict, Union, List
 
 import allure
 import requests
+from allure_commons._allure import StepContext
 from jto import JTOConverter
 from requests import Response
 from trest.curl_converter import CurlConverter
@@ -29,6 +30,8 @@ class RESTRequest(Generic[T]):
                  timeout: float = None):
         self._log.debug(f'function "{inspect.currentframe().f_code.co_name}" '
                         f'called with args "{inspect.getargvalues(inspect.currentframe()).locals}"')
+        self._allure_step = None
+
         self.method = method
         self.url = url
         self.params = params
@@ -85,6 +88,41 @@ class RESTRequest(Generic[T]):
                             response_body=str(response.text),
                             elapsed_time=str(response.elapsed))
 
+    def _log_request(self, to_allure: bool = True) -> None:
+        self._log.debug(f'function "{inspect.currentframe().f_code.co_name}" '
+                        f'called with args "{inspect.getargvalues(inspect.currentframe()).locals}"')
+        curl_request = self.get_curl_string()
+
+        self._log.debug(f'Request curl:\n{curl_request}')
+
+        if Config.print_request_to_std_out:
+            print(curl_request)
+
+        if to_allure:
+            self._allure_step = allure.step(f'{self.method} {self.url}')
+            with self._allure_step:
+                try:
+                    allure.attach(curl_request, 'Request curl', allure.attachment_type.TEXT)
+                except Exception as e:
+                    self._log.warning('Failed to attach request to allure report', stack_info=True)
+
+    def _log_response(self, rest_response, to_allure: bool = True) -> None:
+        self._log.debug(f'function "{inspect.currentframe().f_code.co_name}" '
+                        f'called with args "{inspect.getargvalues(inspect.currentframe()).locals}"')
+        response_repr = rest_response.get_string_repr(one_line=Config.one_line_response)
+
+        self._log.debug(f'Response string:\n{response_repr}')
+
+        if Config.print_response_to_std_out:
+            print(response_repr)
+
+        if to_allure:
+            with self._allure_step:
+                try:
+                    allure.attach(response_repr, 'Response', allure.attachment_type.TEXT)
+                except Exception as e:
+                    self._log.warning('Failed to attach response to allure report', stack_info=True)
+
     def send(self, to_allure: bool = True) -> 'RESTResponse':
         """
         Sends the request and returns the response.
@@ -93,47 +131,7 @@ class RESTRequest(Generic[T]):
         """
         self._log.debug(f'function "{inspect.currentframe().f_code.co_name}" '
                         f'called with args "{inspect.getargvalues(inspect.currentframe()).locals}"')
-        with allure.step(f'{self.method} {self.url}'):
-            curl_request = self.get_curl_string()
-            try:
-                allure.attach(curl_request, 'Request curl', allure.attachment_type.TEXT)
-            except Exception as e:
-                self._log.warning('Failed to attach request to allure report', stack_info=True)
-            self._log.debug(f'Request curl:\n{curl_request}')
-            if Config.print_request_to_std_out:
-                print(curl_request)
-            try:
-                response = requests.request(self.method,
-                                            self.url,
-                                            params=self.params,
-                                            headers=self.headers,
-                                            data=self._convert_body_to_string() if self.body else None,
-                                            hooks=self.hooks,
-                                            timeout=self.timeout)
-
-                rest_response = self._create_response(response)
-                response_repr = rest_response.get_string_repr(one_line=Config.one_line_response)
-                try:
-                    allure.attach(response_repr, 'Response', allure.attachment_type.TEXT)
-                except Exception as e:
-                    self._log.warning('Failed to attach response to allure report', stack_info=True)
-                self._log.debug(f'Response string:\n{response_repr}')
-                if Config.print_response_to_std_out:
-                    print(response_repr)
-
-                return rest_response
-            except Exception as e:
-                raise e
-
-    def _send_request(self):
-        self._log.debug(f'function "{inspect.currentframe().f_code.co_name}" '
-                        f'called with args "{inspect.getargvalues(inspect.currentframe()).locals}"')
-
-        curl_request = self.get_curl_string()
-        self._log.debug(f'Request curl:\n{curl_request}')
-        if Config.print_request_to_std_out:
-            print(curl_request)
-
+        self._log_request(to_allure=to_allure)
         try:
             response = requests.request(self.method,
                                         self.url,
@@ -143,12 +141,7 @@ class RESTRequest(Generic[T]):
                                         hooks=self.hooks,
                                         timeout=self.timeout)
             rest_response = self._create_response(response)
-
-            response_repr = rest_response.get_string_repr(one_line=Config.one_line_response)
-            self._log.debug(f'Response string:\n{response_repr}')
-            if Config.print_response_to_std_out:
-                print(response_repr)
-
+            self._log_response(rest_response, to_allure=to_allure)
             return rest_response
         except Exception as e:
             raise e
